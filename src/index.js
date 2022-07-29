c701663e6a700972b89f180dc0f08e85b93367e5
carlafaes <carlafaes18@hotmail.com>
2022-07-28 23:42:23 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import dotenv from 'dotenv';
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
